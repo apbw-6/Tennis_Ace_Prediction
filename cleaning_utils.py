fb139739e62bfd9673e8b54846f9f69c398379b6
apbw-6 <111440299+apbw-6@users.noreply.github.com>
2025-02-06 17:42:49 +0000

    Did some data cleaning

--- cleaning_utils.py
+++ cleaning_utils.py
@@ -3,5 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+baseline_x = 11.885
+service_line_x = 6.4
+singles_sideline_y = 4.115 
+
 def clean_dataframe(df):
-    
+    return
