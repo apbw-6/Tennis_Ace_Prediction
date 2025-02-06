6fa44fde4343106cbafaf606d9be858741c18cf8
apbw-6 <111440299+apbw-6@users.noreply.github.com>
2025-02-06 19:08:04 +0000

    Finished data cleaning

--- clean_df.py
+++ clean_df.py
@@ -1,11 +1,32 @@
 import pandas as pd
 import numpy as np
+from sklearn.impute import SimpleImputer
 
-baseline_x = 11.885
-service_line_x = 6.4
-singles_sideline_y = 4.115 
+
 
 def clean_dataframe(df):
+    """
+    - z-coordinate cannot be negative.
+    - It is not possible that the server and the receiver are on the same side of the court. These points must be deleted.
+    - It is not possible that the server and the ball bounce is on the same side of the court. These points must be deleted.
+    - <b>Fliping the court</b> to make things simpler. I.e the server will always be on the left and the returner will always be on the right. This shouldn't make any difference to the game.
+    - Server position must be close to the baseline and cannot be way inside the court or way behind.
+    - Returner position must be near or behind the baseline and cannot be way inside the court.
+     Fastest serve speed recorded is 163.7mph. Serve speed cannot exceed this.
+    - Impute missing data with mean and most frequent.
+
+    Args:
+        datafarme: to be cleaned
+
+    Returns:
+        dataframe: cleaned data
+    """
+    
+    # Tennis court dimensions
+    baseline_x = 11.885
+    service_line_x = 6.4
+    singles_sideline_y = 4.115 
+    
     # z-coordinates cannot be negative at all.
     df = df[(df["ball_hit_z"] >= 0) & (df["ball_net_z"] >= 0)]
     
@@ -75,7 +96,19 @@ def clean_dataframe(df):
     # It is not possible for ball speed to be greater at the net than when the server has just hit the ball. These points must be deleted.
     df = df[df["ball_hit_v"] > df["ball_net_v"]]    
     
+    # Create an imputer that fills missing values with the most frequent category
+    imputer = SimpleImputer(strategy="most_frequent")
+
+    cols_most_frequent = [
+        "surface",
+        "serve_side",
+        "serve_number",
+        "hitter_hand",
+        "receiver_hand",
+    ]
+    df[cols_most_frequent] = imputer.fit_transform(df[cols_most_frequent])
     
-    
+    # Impute missing values with column mean (only for numeric columns)
+    df.fillna(df.mean(numeric_only=True), inplace=True)
     
     return df
